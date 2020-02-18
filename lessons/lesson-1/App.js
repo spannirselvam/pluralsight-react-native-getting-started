2eb7d745c7076fbeedf8f4553782f94d11b31efa
Sathy Pannirselvam <spannirselvam@us.ci.org>
2020-02-17 22:50:44 -0700

    lesson1

--- lessons/lesson-1/App.js
+++ lessons/lesson-1/App.js
@@ -9,8 +9,9 @@ YellowBox.ignoreWarnings([
 
 export default class App extends React.Component {
   render() {
-    return (
+     return (
       <View style={styles.container}>
+        <Text>Hello!</Text>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
